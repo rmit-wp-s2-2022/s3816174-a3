dc7b3f4b72a2a31595dba0cdb26b039c47fb9bb5
CalvinSSSSSS <116151588+CalvinSSSSSS@users.noreply.github.com>
2022-10-28 04:51:37 +1100

    Add files via upload

---
+++ a3/footer.php
@@ -0,0 +1,24 @@
+<div class="footer-clean">
+    <footer>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 col-md-4 item">
+                    <h3>Assignment 3</h3>
+                    <ul>
+                        <li><a href="./index.php">All Courses</a></li>
+                        <li><a href="./create.php">Create Student</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-4 item">
+                    <h3>Repository</h3>
+                    <ul>
+                        <li><a href="https://github.com/rmit-wp-s2-2022/s3816174-a3">Github</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 item social">
+                    <p class="copyright">Shiuyan Sun © 2022</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
