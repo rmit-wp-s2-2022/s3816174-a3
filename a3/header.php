9d40c34be066e3446e7e3810f209668881d2ebb3
CalvinSSSSSS <116151588+CalvinSSSSSS@users.noreply.github.com>
2022-10-28 04:44:18 +1100

    Add files via upload

---
+++ a3/header.php
@@ -0,0 +1,7 @@
+<header>
+    <title><?php global $title; echo $title;?></title>
+    <script src="./static/jquery.min.js"></script>
+    <script src="./static/bootstrap.min.js"></script>    
+    <link href="./static/bootstrap.min.css" rel="stylesheet">
+    <link href="./static/a3.css" rel="stylesheet"/>
+</header>
